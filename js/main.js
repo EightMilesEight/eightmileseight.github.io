e3d458a0187a4c4f5f21ce90c0a1124d60902b2e
miles <spikenopaa@gmail.com>
2023-06-05 12:33:17 -0400

    Started to build actual content structure

--- js/main.js
+++
@@ -1,6 +0,0 @@
-/* Preloader */
-document.querySelector(".preloader").style.display = "block";
-function hidePreloader() {
-    document.querySelector(".preloader").style.opacity = "0";
-    document.querySelector(".preloader").style.display = "none";
-}
--- star-generator.py
+++ star-generator.py
@@ -5,8 +5,8 @@ d = 0
 while d < 3000:
     s = random.randint(1, 3)
     t = random.randint(290, 310)
-    r = random.randint(20, 25)
+    r = random.randint(30, 35)
     
-    print('        <div class="star" style="--s:' + str(s) + 'px; --t:' + str(t) + 's; --d:-' + str(d) + 's; --r:-' + str(r) + 'vh"></div>')
+    print('        <div class="star hero-star" style="--s:' + str(s) + 'px; --p:' + str(t) + 's; --d:-' + str(d) + 's; --r:-' + str(r) + 'vh"></div>')
     
     d += 30
